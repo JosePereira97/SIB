90f82c7c9942d919dc6f6f5d339a7efb997234e3
JosePereira97 <74066223+JosePereira97@users.noreply.github.com>
2022-01-16 17:44:54 +0000

    Final review

--- src/si/data/__init__.py
+++
@@ -1,3 +0,0 @@
-from .dataset import Dataset, summary
-from .scale import StandardScaler
-from .feature_selection import VarianceThreshold, SelectKBest
--- src/si/util/__init__.py
+++
@@ -1,4 +0,0 @@
-from .util import *
-from .metrics import *
-from .cv import CrossValidationScore, GridSearchCV
-from .activation import *
